a8c89ee5594e962760ed37dde3a01f1ff043e58e
harsh589459 <hs589459@gmail.com>
2023-09-25 19:58:57 +0530

    styling and using jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,21 @@
-function ExpenseItem() {
-    return <div>
-        <div>March 28th 2021</div>
-        <div>
+import './ExpenseItem.css'
 
-            <h2>Car Insurance</h2>
-            <div>$294.67</div>    </div>
+function ExpenseItem() {
+
+    const expenseDate = new Date(2023,9,25);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
+
+
+    return (<div className="expense-item">
+        <div>{expenseDate.toISOString()}</div>
+        <div className="expense-item__description">
+            <h2>{expenseTitle}</h2>
+            <div className="expense-item__price">${expenseAmount}</div>    
+        </div>
     </div>
+    )
 }
 
 export default ExpenseItem;
