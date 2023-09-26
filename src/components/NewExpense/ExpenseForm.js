b354399a71ce90515c1d6325eaaeb47d77357170
harsh589459 <hs589459@gmail.com>
2023-09-26 17:18:38 +0530

    state inside new component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './NewExpense.css';
 
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount,setEnteredAmount]=useState('');
+    const [enteredDate,setEnteredDate] = useState('');
 
     const tilteChangeHandler = (event)=>{
-        console.log(event.target.value)
+        setEnteredTitle(event.target.value);
     }
+    const AmountChangeHandler=(event)=>{
+        setEnteredAmount(event.target.value);
+
+    }
+    const DateChangeHandler=(event)=>{
+        setEnteredDate(event.target.value);
+
+    }
+
+
+
   return (
     <form>
         <div className='new-expense__controls'>
@@ -17,13 +31,13 @@ const ExpenseForm = () => {
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type='number' min="0.01" step="0.01"/>
+                <input type='number' min="0.01" step="0.01"onChange={AmountChangeHandler}/>
             </div>
         </div>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type='date' min="2019-01-01" max="2028-12-31"/>
+                <input type='date' min="2019-01-01" max="2028-12-31" onChange={DateChangeHandler}/>
             </div>
         </div>
         <div className='new-expense__actions'>
