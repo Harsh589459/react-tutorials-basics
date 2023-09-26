11db9cbc4af02f232edabfc3deff0a83eac20ab1
harsh589459 <hs589459@gmail.com>
2023-09-26 13:56:19 +0530

    lets get started with react completedd

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,22 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
-
-function ExpenseItem(props) {
-
+import Card from '../UI/Card'
 
 
-    return (<div className="expense-item">
+import React from 'react'
+
+const ExpenseItem = (props) => {
+ 
+    return (<Card className="expense-item">
         <ExpenseDate date={props.date}/>
         <div className="expense-item__description">
             <h2>{props.title}</h2>
             <div className="expense-item__price">${props.amount}</div>    
         </div>
-    </div>
+    </Card>
     )
 }
+
+
 
 export default ExpenseItem;
